d14aa024b94fabf0ad7518eab68822c38b321a77
Sam Zoepke <sam.zoepke@gmail.com>
2025-06-10 15:01:02 +0200

    Added sorted function to packing list

--- src/PackingList.jsx
+++ src/PackingList.jsx
@@ -1,15 +1,28 @@
 import Item from "./Item"
+import { useState } from "react"
 
 export default function PackingList({itemList, onHandleToggle, onDelete}){
-
+    const [sortedList, setSortedList] = useState('input')
+    
+    let sortedItems;
+    if (sortedList === 'input') sortedItems = itemList;
+    if (sortedList === "description") sortedItems = itemList.slice().sort((a,b)=>a.description.localeCompare(b.description))
+    if (sortedList === "packed") sortedItems = itemList.slice().sort((a,b)=>Number(a.packed)-Number(b.packed))
     return(
         <div className="list">
         <ul>
-            {itemList.map((item)=>
+            {sortedItems.map((item)=>
             <Item item={item} key={item.id} onHandleToggle={onHandleToggle} onDelete={onDelete}/>
-            )}
-            
+            )} 
         </ul>
+        <div className="actions">
+        <select value={sortedList} onChange={(e)=>setSortedList(e.target.value)}>
+            <option value="input">Sort by input order</option>
+            <option value="description">Sort by description</option>
+            <option value="packed">Sort by packed status</option>
+        </select>
+        <button>Clear List </button>
+        </div>
         </div>    
     )
 }
